bc7fcd20e4ab60653217e09655e60c58fb2dcfbb
Ved Prakash Pandey <ved526543@gmail.com>
2025-07-31 01:31:25 +0530

    made header,hero section,new arrivals,featured products

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,101 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../assets/images/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import Slide1 from '../assets/images/banner-1.jpg';
+import Slide2 from '../assets/images/banner-2.jpg';
+import LatestProducts from './common/LatestProducts';
+import FeaturedProducts from './common/FeaturedProducts';
 
 const home = () => {
   return (
+   <>
    <header className='shadow'>
-    <button className='btn btn-primary'>Click Me</button>
-   </header>
+      <div className='bg-dark text-center py-3'>
+        <span className='text-white'>Our Fashion Partners</span>
+      </div>
+      {/* navbar */}
+
+      <div className="container ">
+        <Navbar expand="lg" className="">
+
+          <Navbar.Brand href="#">
+            <img src={Logo} alt="Theradora Logo" width={170} height={75} className='' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="ms-auto my-2 my-lg-0"
+
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Men</Nav.Link>
+              <Nav.Link href="#action2">Women</Nav.Link>
+              <Nav.Link href="#action2">Kids</Nav.Link>
+
+
+            </Nav>
+
+
+            <div className="nav-right d-flex">
+              <a href="" className='ms-3'>
+                <FontAwesomeIcon icon={faUser} />
+              </a>
+              <a href="" className='ms-3'>
+                <FontAwesomeIcon icon={faCartShopping} />
+
+              </a>
+
+            </div>
+          </Navbar.Collapse>
+
+        </Navbar>
+
+      </div>
+
+
+    </header>
+    
+    
+    <section className="section-1">
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={1}
+          breakpoints={{
+            1024: {
+              slidesPerView: 1,
+              spaceBetween: 0,
+            }
+          }}
+        >
+          <SwiperSlide>
+            <div className="content" style={{ backgroundImage: `url(${Slide1})` }}
+            >
+
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="content" style={{ backgroundImage: `url(${Slide2})` }}
+            >
+
+            </div>
+          </SwiperSlide>
+
+        </Swiper>
+      </section>
+      
+      <LatestProducts/>
+      <FeaturedProducts/>
+      
+      
+      </>
   )
 }
 
---
+++ frontend/src/components/common/LatestProducts.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import Product1 from '../../assets/images/mens/1.jpg';
+
+const LatestProducts = () => {
+  return (
+   
+        <section className="section-2 py-5">
+        <div className="container">
+          <h2>New Arrivals</h2>
+
+          <div className="row mt-4">
+            <div className="col-md-3">
+              <div className="product card border-0">
+                <div className="card-img">
+                      <img src={Product1} alt="productimage" className='w-100' />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">
+                  <p>Men's Check Shirt</p>
+                  </a>
+
+                  <div className="price">
+                    $50 <s>$80</s>
+                  </div>
+                </div>
+
+                
+              </div>
+
+              
+            </div>
+
+                     <div className="col-md-3">
+              <div className="product card border-0">
+                <div className="card-img">
+                      <img src={Product1} alt="productimage" className='w-100' />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">
+                  <p>Men's Check Shirt</p>
+                  </a>
+
+                  <div className="price">
+                    $50 <s>$80</s>
+                  </div>
+                </div>
+
+                
+              </div>
+
+              
+            </div>
+
+                     <div className="col-md-3">
+              <div className="product card border-0">
+                <div className="card-img">
+                      <img src={Product1} alt="productimage" className='w-100' />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">
+                  <p>Men's Check Shirt</p>
+                  </a>
+
+                  <div className="price">
+                    $50 <s>$80</s>
+                  </div>
+                </div>
+
+                
+              </div>
+
+              
+            </div>
+
+                     <div className="col-md-3">
+              <div className="product card border-0">
+                <div className="card-img">
+                      <img src={Product1} alt="productimage" className='w-100' />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">
+                  <p>Men's Check Shirt</p>
+                  </a>
+
+                  <div className="price">
+                    $50 <s>$80</s>
+                  </div>
+                </div>
+
+                
+              </div>
+
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    
+  )
+}
+
+export default LatestProducts
